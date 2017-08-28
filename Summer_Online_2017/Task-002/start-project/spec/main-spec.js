e71cb6164f2976cde0d02c1fa417d55ff74ed880
FrankBATMAN <FrankBATMAN@126.com>
2017-08-28 10:49:33 +0800

    delete all

--- Summer_Online_2017/Task-002/start-project/spec/main-spec.js
+++
@@ -1,9 +0,0 @@
-const add = require('../main/main');
-
-describe('main()', () => {
-
-    it('should return added result', () => {
-        let actual = add(1,1);
-        expect(actual).toBe(2);
-    });
-});
